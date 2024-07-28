1764fc7ceb1b6d99bc11112f1ddd5ff0ed5242bc
M MADHU KALYAN <raydriiger@gmail.com>
2024-07-29 06:13:47 +1200

    fixed eureka related issues

--- currency-conversion/src/main/java/org/currencyconversion/CurrencyConversionApplication.java
+++ currency-conversion/src/main/java/org/currencyconversion/CurrencyConversionApplication.java
@@ -2,13 +2,10 @@ package org.currencyconversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaServer
-
 public class CurrencyConversionApplication {
 
     public static void main(String[] args) {
--- currency-exchange-service/src/main/java/org/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/org/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,11 +2,8 @@ package org.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
